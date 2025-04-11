8d9950419facbffbbe5bd5b9d176263b7bfee449
ksljivo1 <ksljivo1@etf.unsa.ba>
2025-04-11 15:08:08 +0200

    implemented full routing setup with Navbar and pages (Home, About, Products, Cart) along with initial CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import React from "react";
 import {createRoot} from "react-dom/client";
-import Input from "./components/Input.jsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Products from "./components/Products.jsx";
+import Cart from "./components/Cart.jsx";
 
 export default function App() {
-    function handleLastNameInput() {
-        console.log("Last name changed");
-    }
-
-    return (<div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
-        <Input placeholder="First name" />
-        <Input placeholder="Last name" onInput={handleLastNameInput} />
-        <Input placeholder="Email" type="email" required />
-    </div>);
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />}>
+                    </Route>
+                    <Route path="/about" element={<About />}>
+                    </Route>
+                    <Route path="/products" element={<Products />}>
+                    </Route>
+                    <Route path="/cart" element={<Cart />}>
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-createRoot(document.querySelector("#root")).render(<App />);
+createRoot(document.querySelector("#root")).render(<React.StrictMode><App /></React.StrictMode>);
